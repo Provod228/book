bcbe3d3d10967f053bb565c2a9beaacfbc24f0bf
Provod228 <tulush.ignat@bk.ru>
2024-06-05 16:11:15 +0300

    mmmmmmm

---
+++ catalog/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+
+
+# Create your views here.
+
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instance = BookInstance.objects.all().count()
+    num_instance_available = BookInstance.objects.filter(status__exact=2).count()
+    num_author = Author.objects.count()
+    return render(request, 'index.html',
+                  context={'num_books': num_books,
+                           'num_instance': num_instance,
+                           'num_instance_available': num_instance_available,
+                           'num_author': num_author,
+                           }
+                  )
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 3
+    template_name = 'book_list.html'
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'book_detail.html'
+    paginate_by = 3
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 4
+    template_name = 'author_list.html'
