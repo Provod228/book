0b7cee684cd46c06a6da1f88b9e432853d59a264
Provod228 <tulush.ignat@bk.ru>
2024-06-29 13:55:17 +0300

    полностью изменены под drf

--- catalog/views.py
+++ catalog/views.py
@@ -1,42 +1,51 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Book, Author, BookInstance, Genre
-from django.views import generic
+from .models import Book, Author, BookInstance
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 # Create your views here.
 
 
-def index(request):
-    num_books = Book.objects.all().count()
-    num_instance = BookInstance.objects.all().count()
-    num_instance_available = BookInstance.objects.filter(status__exact=2).count()
-    num_author = Author.objects.count()
-    num_visits = request.session.get('num_visits', 0)
-    request.session['num_visits'] = num_visits + 1
-    return render(request, 'index.html',
-                  context={'num_books': num_books,
-                           'num_instance': num_instance,
-                           'num_instance_available': num_instance_available,
-                           'num_author': num_author,
-                           'num_visits': num_visits,
-                           }
-                  )
+class index(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'index.html'
+
+    def get(self, request):
+        num_books = Book.objects.all().count()
+        num_instance = BookInstance.objects.all().count()
+        num_instance_available = BookInstance.objects.filter(status__exact=2).count()
+        num_author = Author.objects.count()
+        return Response({
+                        'num_books': num_books,
+                        'num_instance': num_instance,
+                        'num_instance_available': num_instance_available,
+                        'num_author': num_author,
+                        })
 
 
-class BookListView(generic.ListView):
-    model = Book
-    paginate_by = 3
+class BookListView(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
     template_name = 'book_list.html'
 
+    def get(self, request):
+        book_list = Book.objects.all()
+        return Response({'book_list': book_list})
 
-class BookDetailView(generic.DetailView):
-    model = Book
+
+class BookDetailView(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
     template_name = 'book_detail.html'
-    paginate_by = 3
+
+    def get(self, request, id):
+        book = Book.objects.get(pk=id)
+        return Response({'book': book})
 
 
-class AuthorListView(generic.ListView):
-    model = Author
-    paginate_by = 4
+class AuthorListView(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
     template_name = 'author_list.html'
+
+    def get(self, request):
+        author_list = Author.objects.all()
+        return Response({'author_list': author_list})
