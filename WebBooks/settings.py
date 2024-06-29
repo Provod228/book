13586843229b26c7abfae767e953b34822d6c4f9
Provod228 <tulush.ignat@bk.ru>
2024-06-29 13:56:01 +0300

    не значительные изменения

--- WebBooks/settings.py
+++ WebBooks/settings.py
@@ -32,13 +32,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'catalog',
+    'catalog.apps.CatalogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
