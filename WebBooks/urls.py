fa290f6e224a813949650ce7c3dca19d7328f77f
Provod228 <tulush.ignat@bk.ru>
2024-07-17 11:16:30 +0300

    изменение urls

--- WebBooks/urls.py
+++ WebBooks/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from catalog import views
 
 
 urlpatterns = [
-    path('', views.index.as_view(), name='index'),
     path('catalog/', include('catalog.urls')),
     path('admin/', admin.site.urls),
 ]
