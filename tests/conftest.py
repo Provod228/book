2c697e1770abd83f390ca3469e07436eebde8c53
Provod228 <tulush.ignat@bk.ru>
2024-10-28 20:21:58 +0300

    add test_serializers

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,3 +4,46 @@ import pytest
 @pytest.fixture(scope='function')
 def test_url(request) -> str:
     return 'http://127.0.0.1:8000/'
+
+
+@pytest.fixture(scope='function')
+def test_book_data() -> dict:
+    return {
+        'id': 1,
+        'title': 'test_title',
+        'genre': 'test_genre',
+        'summary': 'test_summary',
+        'isbn': 'test_isbn',
+        'language': 'test_language',
+    }
+
+
+@pytest.fixture(scope='function')
+def test_count_data() -> dict:
+    return {
+       'num_books': 1,
+       'num_instance': 1,
+       'num_instance_available': 1,
+       'num_author': 1,
+    }
+
+
+@pytest.fixture(scope='function')
+def test_author_data() -> dict:
+    return {
+        'first_name': "test_first_name",
+        'last_name': "test_last_name",
+        'data_of_birth': "1234-01-01",
+        'data_of_death': "1265-01-01",
+    }
+
+
+@pytest.fixture(scope='function')
+def test_book_instance_data() -> dict:
+    return {
+        'status_display': 1,
+        'imprint': "test_imprint",
+        'id': 1,
+        'status': "test_status",
+    }
+
--- tests/test_catalog/test_serializers.py
+++ tests/test_catalog/test_serializers.py
@@ -1,4 +1,34 @@
-import pytest
 from catalog.serializers import *
+from catalog.models import *
+
+
+def test_index_serializers(test_count_data):
+    serializer = IndexSerializer(data=test_count_data)
+    assert serializer.is_valid()
+
+
+def test_book_list_serializers(test_book_data):
+    serializer = BookListSerializers(data=test_book_data)
+    assert serializer.is_valid()
+
+
+def test_book_detail_serializers(test_book_data):
+    serializer = BookDetailSerializers(data=test_book_data)
+    assert serializer.is_valid()
+
+
+def test_book_author_detail_serializers(test_author_data):
+    serializer = BookAuthorDetailSerializers(data=test_author_data)
+    assert serializer.is_valid()
+
+
+def test_author_list_serializers(test_author_data):
+    serializer = AuthorListSerializers(data=test_author_data)
+    assert serializer.is_valid()
+
+
+def test_book_instance_detail_serializers(test_book_instance_data):
+    serializer = BookInstanceDetailSerializers(data=test_book_instance_data)
+    assert serializer.is_valid()
 
 
