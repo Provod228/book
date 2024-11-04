733d87b1cc884d14f523803fd57a3e491d717232
Provod228 <tulush.ignat@bk.ru>
2024-11-04 22:03:57 +0300

    add test_views.py

--- tests/test_catalog/test_views.py
+++ tests/test_catalog/test_views.py
@@ -1,5 +1,15 @@
 import pytest
+from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework.reverse import reverse
 
 
+class TestIndexAPIView(TestCase):
+    def setUp(self):
+        self.client = APIClient()
 
+        print(self.client, "self.client")
+
+    def test_index_get(self):
+        pass
 
