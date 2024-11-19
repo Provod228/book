bc008c0549cb7b62f3a4fab6b6a59a634c1eda7f
Provod228 <tulush.ignat@bk.ru>
2024-11-19 18:45:48 +0300

    all the tests are working

--- tests/test_catalog/test_views.py
+++ tests/test_catalog/test_views.py
@@ -1,6 +1,5 @@
 import pytest
-from catalog.models import Book, BookInstance, Author
-from catalog.urls import *
+from catalog.models import Author, Book, BookInstance
 from mixer.backend.django import mixer
 from rest_framework.test import APIClient
 from rest_framework.reverse import reverse
@@ -9,7 +8,7 @@ from django.test import TestCase
 
 @pytest.mark.django_db
 class TestIndexAPIView(TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.client = APIClient()
 
     def test_index_list_works(self) -> None:
@@ -18,7 +17,9 @@ class TestIndexAPIView(TestCase):
         mixer.blend(BookInstance)
         mixer.blend(BookInstance)
 
-        response = self.client.get(reverse('catalog:index'))
+        url = reverse('catalog:index')
+
+        response = self.client.get(url)
 
         assert response.json != None
         assert len(response.data) == 4
@@ -31,7 +32,7 @@ class TestIndexAPIView(TestCase):
 
 @pytest.mark.django_db
 class TestBookListView(TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.client = APIClient()
 
     def test_book_list_works(self) -> None:
@@ -40,7 +41,9 @@ class TestBookListView(TestCase):
         mixer.blend(Book)
         mixer.blend(Book)
 
-        response = self.client.get(reverse('catalog:books'))
+        url = reverse('catalog:books')
+
+        response = self.client.get(url)
 
         assert response.json != None
         assert len(response.data['book_list']) == 4
@@ -50,31 +53,31 @@ class TestBookListView(TestCase):
 
 @pytest.mark.django_db
 class TestBookDetailView(TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.client = APIClient()
 
         print(self.client, "self.client")
 
     def test_book_detail_list_works(self) -> None:
-        mixer.blend(Book)
-        # mixer.blend(Book)
-        # mixer.blend(Book)
-        # mixer.blend(Book)
+        author = mixer.blend(Author)
+        book = mixer.blend(Book, author=author)
+        mixer.blend(BookInstance, book=book)
 
-        print(Book.objects.all().values_list()[0][0])
+        url = reverse('catalog:book-detail', kwargs={"id": book.id})
 
-        response = self.client.get(reverse('catalog:book-detail'))
+        response = self.client.get(url)
 
-        # Потом сделаю нормальные assert
-        # assert response.json != None
-        # assert len(response.data['book_list']) == 4
-        # assert len(response.data['book_list'][0]) == 3
-        # assert response.status_code == 200
+        assert response.json != None
+        assert len(response.data['book']) == 5
+        assert len(response.data['authors'][0]) == 2
+        assert len(response.data['book_instance_set'][0]) == 3
+        assert len(response.data) == 3
+        assert response.status_code == 200
 
 
 @pytest.mark.django_db
 class TestAuthorListView(TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.client = APIClient()
 
     def test_author_list_works(self) -> None:
@@ -83,7 +86,9 @@ class TestAuthorListView(TestCase):
         mixer.blend(Author)
         mixer.blend(Author)
 
-        response = self.client.get(reverse('catalog:authors'))
+        url = reverse('catalog:authors')
+
+        response = self.client.get(url)
 
         assert response.json != None
         assert len(response.data['author_list']) == 4
