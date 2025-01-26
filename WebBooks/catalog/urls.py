754952fc5fcfae55802f3c02148e240b91a243ae
Provod228 <tulush.ignat@bk.ru>
2025-01-26 20:00:47 +0300

    add 2 url for add

--- WebBooks/catalog/urls.py
+++ WebBooks/catalog/urls.py
@@ -6,7 +6,9 @@ app_name = 'catalog'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('books/', BookListView.as_view(), name='books'),
+    path('books/add', BookAddView.as_view(), name='book_add'),
     path('books/<int:id>', BookDetailView.as_view(), name='book-detail'),
+    path('authors/add', AuthorAddView.as_view(), name='author_add'),
     path('authors/', AuthorListView.as_view(), name='authors'),
     path('accounts/signup', UserRegistrationView.as_view(), name='signup'),
     path('accounts/', include('django.contrib.auth.urls')),
